b770c87d1f0b9b8c769ab61f5dc696886b24e720
JustinJefferson <justleland@gmail.com>
2020-01-22 12:53:55 -0500

    Created models folder with a monster model.

---
+++ src/app/models/monster.ts
@@ -0,0 +1,16 @@
+export class Monster{
+    monsterId: number;
+    monsterName: string;
+    description: string;
+    familyIf: number;
+    hp: number;
+    sp: number;
+    atk: number;
+    def: number;
+    strength: number;
+    dexterity: number;
+    intelligence: number;
+    agility: number;
+    luck: number;
+    preEvolutionId: number;
+}
